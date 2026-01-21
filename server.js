85fdab4f34fcf58a5473b379fc057960d3568609
Maha Khan <mahrukh.khan266@gmail.com>
2026-01-21 10:54:01 +0500

    Work Complete

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended : false }));
+
+app.get("/custom_api", (req, res) => {
+    res.send({
+        "completed" : true,
+        "count" : 46
+    })
+}).listen(3700);
+
